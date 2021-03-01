23e1e14bfbdcaf520f335011b007ee2735504a07
koualsky <koualsky@gmail.com>
2021-03-01 13:06:14 +0100

    Fixed post method

--- app/views.py
+++ app/views.py
@@ -22,12 +22,12 @@ class HelloView(APIView):
 
 class CreateUserView(CreateAPIView):
     model = get_user_model()
-    permission_classes = (AllowAny,)
+    # permission_classes = (AllowAny,)
     serializer_class = UserSerializer
 
 
 class GeolocalizationView(APIView):
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
 
     def post(self, request):
 
@@ -48,31 +48,33 @@ class GeolocalizationView(APIView):
             f"http://api.ipstack.com/{address}?access_key={settings.GEOLOCALIZATION_KEY}&format=1"
         )
         content = json.loads(r.text)
-        obj, created = Geolocalization.objects.get_or_create(
-            ip=content.get("ip"),
-            type=content.get("type"),
-            continent_code=content.get("continent_code"),
-            continent_name=content.get("continent_name"),
-            country_code=content.get("country_code"),
-            country_name=content.get("country_name"),
-            region_code=content.get("region_code"),
-            region_name=content.get("region_name"),
-            city=content.get("city"),
-            zip=content.get("zip"),
-            latitude=content.get("latitude"),
-            longitude=content.get("longitude"),
-            location=content.get("location"),
-        )
-
-        # Response
-        if created:
-            return Response({"message": f"IP {address} was saved to the database."})
-        else:
+        ip_exists = Geolocalization.objects.get(ip=content.get("ip"))
+        if ip_exists:
             return Response(
                 {
                     "message": f"Geolocalization with this ip ({address}) already exists in the database."
                 }
             )
+        else:
+            try:
+                Geolocalization.objects.create(
+                    ip=content.get("ip"),
+                    type=content.get("type"),
+                    continent_code=content.get("continent_code"),
+                    continent_name=content.get("continent_name"),
+                    country_code=content.get("country_code"),
+                    country_name=content.get("country_name"),
+                    region_code=content.get("region_code"),
+                    region_name=content.get("region_name"),
+                    city=content.get("city"),
+                    zip=content.get("zip"),
+                    latitude=content.get("latitude"),
+                    longitude=content.get("longitude"),
+                    location=content.get("location"),
+                )
+                return Response({"message": f"IP {address} was saved to the database."})
+            except Exception as e:
+                return Response({"message": e})
 
     def get(self, request):
         address = request.GET.get("address")
