69013c37fbd4f1bff3ffe2e035469be2fbec75bd
koualsky <koualsky@gmail.com>
2021-03-02 13:36:16 +0100

    Changed token url

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt import views as jwt_views
 from . import views
 
 urlpatterns = [
-    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.CreateUserView.as_view(), name='register'),
     path('hello/', views.HelloView.as_view(), name='hello'),
