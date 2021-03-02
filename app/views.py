9376d3df26bc7d0a59de11db10c9e35cdd020e27
koualsky <koualsky@gmail.com>
2021-03-02 13:35:27 +0100

    Removed unnecessary import

--- app/views.py
+++ app/views.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.generics import CreateAPIView
-from rest_framework import status
 from django.contrib.auth import get_user_model
 from django.conf import settings
 import socket
